12afb56ea6be887f25a7d51e2222334e4266d717
Maroon20 <marwen.ghodben@sesame.com.tn>
2021-05-14 15:26:20 +0000

    second commit

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Marwen')
-    print('how are you')
+    print('how are you today')
+    print('good for you')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
